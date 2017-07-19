b16e6ab8499673722ca3cff389c363d8c0f8cbe2
Steffan Sluis <steffansluis@gmail.com>
2017-07-19 17:53:38 +0200

    Fix lib folder

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,31 +1,15 @@
-declare const Producer: any, SimpleConsumer: any, GroupConsumer: any, LATEST_OFFSET: any, EARLIEST_OFFSET: any;
-declare const Observable: any, Subject: any, ReplaySubject: any;
-declare const PQueue: any;
-declare const OFFSET_COMMIT_INTERVAL = 1000;
-declare const RETENTION_TIME: number;
-declare const log: (...things: any[]) => void;
-declare const onError: (error: any) => void;
-declare const isQuad: (quad: any) => boolean;
-declare const isAction: (action: any) => boolean;
-declare const isProgress: (progress: any) => boolean;
+import { Subject } from '@reactivex/rxjs';
 declare const memux: ({url, name, input, output}: {
     url: any;
     name: any;
     input?: null;
     output?: null;
 }) => {
-    source: any;
-    sink: any;
+    source: Subject<{}>;
+    sink: Subject<{}>;
     send: ({type, quad}: {
         type: any;
         quad: any;
-    }) => any;
+    }) => void | Promise<void>;
 };
-declare const createSource: (connectionString: any, groupId: any, topic: any) => {
-    source: any;
-    sink: any;
-};
-declare const createSend: (connectionString: any, label: any, topic: any) => ({type, quad}: {
-    type: any;
-    quad: any;
-}) => any;
+export default memux;
