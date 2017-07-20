439b87a5a66cebe09edef513c65e89a0d0d67d5e
Steffan Sluis <steffansluis@gmail.com>
2017-07-20 15:14:36 +0200

    Test Kafka connection

--- test/index.js
+++ test/index.js
@@ -14,3 +14,18 @@ test('it returns a sink', t => {
   const res = memux({});
   t.is('sink' in res, true);
 });
+
+test('it should be able to connect to Kafka', t => {
+  const url = 'localhost:9092';
+  const output = 'mock_output_topic';
+  const name = 'test';
+
+  const log = console.log.bind(console);
+  const error = console.error.bind(console);
+
+  const { send } = memux({ url, output, name });
+  const quad = { subject: '', predicate: '', object: '' };
+  const action = { type: 'write', quad };
+
+  return (async () => send(action))().then(() => t.pass(true), t.fail);
+});
